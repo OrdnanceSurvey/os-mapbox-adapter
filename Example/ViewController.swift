a6d3f27ddce2351f09d0922c2b3b8867cb759623
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2016-05-04 10:32:33 +0100

    Add swift example

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -8,14 +8,24 @@
 
 import UIKit
 import Mapbox
+import OSMapboxAdapter
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MGLMapView!
 
+    var apiKey: String {
+        return NSBundle.mainBundle().URLForResource("APIKEY", withExtension: nil).flatMap { url -> String? in
+            do { return try String(contentsOfURL: url).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) } catch { return nil }
+            } ?? ""
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        OSMapsAPI.setAPIKey(apiKey)
+        mapView.styleURL = OSMapsAPI.stylesheetURLForProduct(.Road)
+        mapView.centerCoordinate = CLLocationCoordinate2D(latitude: 50.9386, longitude: -1.4705)
+        mapView.zoomLevel = 10
     }
 
 }
